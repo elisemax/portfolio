5e51a3813978189acd268712d89fc9d48baf3137
Max <max11cz@seznam.cz>
2022-06-15 22:00:29 +0200

    add links fix dide panel add

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,9 @@
 const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
-    closeMenu = document.querySelector('.menu__close');
+    closeMenu = document.querySelector('.menu__close'),
+    sidePanel = document.querySelector('.sidepanel'),
+    sidePanelDivider = document.querySelector('.sidepanel__divider');
+const sidePanelLink = document.querySelectorAll('.sidepanel__link');
 hamburger.addEventListener('click',()=>{
     menu.classList.add('active');
 });
@@ -12,3 +15,21 @@ const raitingCounters = document.querySelectorAll('.skills__ratings-counter'),
 raitingCounters.forEach((item,index) => {
     lines[index].style.width = item.innerHTML;
 });
+window.onscroll = function(){
+    const scr = window.scrollY;
+    if(scr >= 500){
+        sidePanel.classList.add('sidepanel__blackText');
+        for (let index = 0; index < sidePanelLink.length; index++) {
+            sidePanelLink[index].classList.add('sidepanel__blackLink')
+        }
+        console.log(sidePanelLink);
+        sidePanelDivider.classList.add('sidepanel__blackDivider');
+    }else{
+        sidePanel.classList.remove('sidepanel__blackText');
+        for (let index = 0; index < sidePanelLink.length; index++) {
+            sidePanelLink[index].classList.remove('sidepanel__blackLink')
+        }
+        sidePanelDivider.classList.remove('sidepanel__blackDivider');
+    }
+}
+
